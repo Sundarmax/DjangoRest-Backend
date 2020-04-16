196128eecf977380cfd1b6ee1b7b9896f128a3c3
sundarmax <sundar.info22@gmail.com>
2020-04-16 14:32:56 +0530

    Skill model updates

--- rest/testapp5/admin.py
+++ rest/testapp5/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from testapp5.models import *
 
 # Register your models here.
+
+admin.site.register(skill_book_name)
+admin.site.register(skill_category)
+admin.site.register(skill_topic)
+admin.site.register(skill)
--- rest/testapp5/models.py
+++ rest/testapp5/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+class skill_category(models.Model):
+
+    skill_category_name   = models.CharField(max_length=155)
+    description           = models.CharField(max_length=255,blank=True,null=True)
+    active                = models.BooleanField(default=True,null=True,blank=True)
+
+    def __str__(self):
+        return self.skill_category_name
+
+    class Meta:
+        db_table = 'skill_category'
+
+class skill_book_name(models.Model):
+
+    skill_book_name =   models.CharField(max_length=155)
+    description     =   models.CharField(max_length=255,blank=True,null=True)
+    skill_category  =   models.ForeignKey(skill_category,related_name="skill_categories", on_delete=models.CASCADE )
+    skill_topics_order = JSONField(null=True,blank=True)
+    active          =   models.BooleanField(default=True,null=True,blank=True)
+
+    def __str__(self):
+        return self.skill_book_name
+    
+    class Meta:
+        db_table = 'skill_book_name'
+
+class skill_topic(models.Model):
+
+    skill_topic_name =   models.CharField(max_length=155)
+    description     =   models.CharField(max_length=255,blank=True,null=True)
+    skill_book      =   models.ForeignKey(skill_book_name,related_name="skill_book_names", on_delete=models.CASCADE )
+    skills_order    =   JSONField(null=True,blank=True)
+    active          =   models.BooleanField(default=True,null=True,blank=True)
+
+    def __str__(self):
+        return self.skill_topic_name
+    
+    class Meta:
+        db_table = 'skill_topic'
+
+class skill(models.Model):
+
+    skill_name      =   models.CharField(max_length=155)
+    description     =   models.CharField(max_length=255,blank=True,null=True)
+    skill_topic     =   models.ForeignKey(skill_topic,related_name="skill_topics", on_delete=models.CASCADE )
+    active          =   models.BooleanField(default=True,null=True,blank=True)
+
+    def __str__(self):
+        return self.skill_name
+    
+    class Meta:
+        db_table = 'skill'
+
+""" class skills_successor(models.Model):
+    skill           =   models.ForeignKey(skill,null=True,related_name='parent_skill',on_delete=models.CASCADE )
+    skill_successor =   models.ForeignKey(skill,null=True,related_name='next_skill',on_delete=models.CASCADE )
+    weightage       =   models.IntegerField(null=True,blank=True)
+
+    class Meta:
+        db_table = 'skills_successor'
+
+ """
